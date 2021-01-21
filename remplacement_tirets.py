85e5c6f574b6e8c1a62e37b60c0b652cf1313e44
Starnix <theo.audurier@gmail.com>
2021-01-21 14:34:09 +0100

    Update remplacement_tirets.py
    Modifications nom de la fonction + ajouts commentaires

--- remplacement_tirets.py
+++ remplacement_tirets.py
@@ -1,14 +1,14 @@
-#définition de la fonction: comptage
-def comptage():
-    #demande a l'utilisateur de rentrer un mot
+def remplacement_tirets():
+    'Fonction permettant de remplacer chaque lettre dun mot par un tiret.'
+    #On demande à l'utilisateur de rentrer un mot.
     mot=input("Donnes un mot:")
+    #On définit une nouvelle variable calcul qui permettra de compter chaque lettre.
     calcul=0
-    #il prend calcul égal à 0 
+    #Boucle qui permet de compter chaque lettre dans un mot.
     for x in mot:
-        #il fait pour une valeur dans mot:
+        #Il incrémente 1 à chaque fois qu'il trouve une nouvelle lettre.
         calcul=calcul+1
-        #il rajoutera +1 à chaque lettres
     return calcul
-    #retourner la fonction
-print("_ " * comptage())
-#faire la fonction
+    #Il renvoie la var calcul qui permettra de recevoir le résultat.
+print("_ " * remplacement_tirets())
+#On multiplie un tiret par le nombre de fois qu'il avait trouvé de lettres dans un mot.
